7a9c5ce22a10038d2c8b440c2b779bbf553a7b18
Henry Swanson <henryswanson94@gmail.com>
2024-08-11 12:32:48 -0700

    Switch GCD algorithm

--- src/main.rs
+++ src/main.rs
@@ -243,13 +243,51 @@ fn is_prime(n: &BigUint) -> bool {
 }
 
 fn gcd(mut a: BigUint, mut b: BigUint) -> BigUint {
-    if a < b {
-        std::mem::swap(&mut a, &mut b);
-    }
+    // Since we're working with a binary representation, it's more efficient
+    // to use this algorithm than the standard Euclidean one.
 
-    while b != BigUint::ZERO {
-        (b, a) = (a % &b, b);
-    }
+    // First, pull out all the factors of 2.
+    // gcd(2^i a, 2^j b) = 2^k gcd(a, b) when a, b odd and k = min(i, j);
+    // But since x.trailing_zeros() is None when x is 0, take this opportunity
+    // to check for the edge case of a = 0 or b = 0.
+    let i = match a.trailing_zeros() {
+        Some(i) => i,
+        None => return b,
+    };
+    let j = match b.trailing_zeros() {
+        Some(j) => j,
+        None => return a,
+    };
 
-    a
+    // How many of those 2s do we want to keep?
+    let k = i.min(j);
+    a >>= i;
+    b >>= j;
+
+    loop {
+        // Now they're both odd
+        debug_assert!(a.bit(0));
+        debug_assert!(b.bit(0));
+
+        // Swap so a is larger
+        if a < b {
+            std::mem::swap(&mut a, &mut b);
+        }
+
+        // Subtract, just like with Euclid
+        // gcd(u, v) = gcd(u, v-u)
+        a -= &b;
+
+        // Now a is even; remove its 2s (again checking for the case of a = 0).
+        // gcd(2^i a, b) = gcd(a, b) when b is odd
+        match a.trailing_zeros() {
+            Some(i) => {
+                a >>= i;
+            }
+            None => {
+                // gcd(0, b) = b, then add in the 2^k we removed earlier
+                return b << k;
+            }
+        }
+    }
 }
